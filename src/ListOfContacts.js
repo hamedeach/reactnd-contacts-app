6a8614eb755e7c738d929a55c13460d6b7a09ca4
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-04-17 11:58:35 +0200

    1-create list of contatct  - react comonenet. 2-dummy contatct list pass it to the component 3- log the contacts list

---
+++ src/ListOfContacts.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class ListOfContacts extends React.Component{
+    render(){
+        console.log(this.props)
+        return(
+            <ol className="contact-list">
+
+            </ol>
+        )
+    }
+
+}
+
+export default ListOfContacts;
