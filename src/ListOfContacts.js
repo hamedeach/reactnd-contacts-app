208bc3c20a5677630b2c801f4ea75efc4254b368
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-04-18 12:00:07 +0200

    statless function component instead of class component

--- src/ListOfContacts.js
+++ src/ListOfContacts.js
@@ -1,40 +1,37 @@
 import React from "react";
 
+// stateless function component instead of the class component 
+function ListOfContacts(props){
+    let lstOfContacts = props['contactsList'];
+    console.log(lstOfContacts);
+    return(
+        <ol className="contact-list">
+            {
+                lstOfContacts.map((person)=>(
+                    <li key={person.id} className="contact-list-item">
 
-class ListOfContacts extends React.Component{
-    render(){
-       
-        let lstOfContacts = this.props['contactsList'];
-        console.log(lstOfContacts);
-        return(
-            <ol className="contact-list">
-                {
-                    lstOfContacts.map((person)=>(
-                        <li key={person.id} className="contact-list-item">
+                        
+                        <div className="contact-avatar" style={{
+                            backgroundImage : `url(${person.avatarURL})`
 
-                            
-                            <div className="contact-avatar" style={{
-                                backgroundImage : `url(${person.avatarURL})`
+                        }}>
+                        </div>
+                        
+                        <div className="contact-details">
+                            <p>{person.name}</p>
+                            <p>{person.handle}</p>
 
-                            }}>
-                            </div>
-                            
-                            <div className="contact-details">
-                                <p>{person.name}</p>
-                                <p>{person.handle}</p>
+                        </div>
+                        <button className="contact-remove">remove</button>
+                    </li>
 
-                            </div>
-                            <button className="contact-remove">remove</button>
-                        </li>
-
-                    ))
+                ))
 
 
-                }
-            </ol>
-        )
-    }
-
+            }
+        </ol>
+    )    
 }
+
 
 export default ListOfContacts;
