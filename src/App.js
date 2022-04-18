abd33a0788a7feba791cf634e4ac816d175ed917
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-04-18 12:35:58 +0200

    applyiing state to the app class

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
 import ListOfContacts from './ListOfContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
-
 class App extends Component {
+  //add the contacts array into state property 
+  state ={
+    contacts : [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+    
+  }
+
   render() {
     return (
       <div>
         <h1>Contatcts App</h1>
-      <ListOfContacts contactsList={contacts}/>
+      <ListOfContacts contactsList={this.state.contacts}/>
       </div>
     );
   }
