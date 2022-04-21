174295568d3c125b67ba81014474b304a27b6c08
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-04-21 07:05:03 +0200

    use the class component instead of stateless function component to use state management feature of the react to handler the search feature

--- src/ListOfContacts.js
+++ src/ListOfContacts.js
@@ -1,49 +1,56 @@
-import React from "react";
+import React,{Component} from "react";
 import propTypes from "prop-types";
 
-// stateless function component instead of the class component 
-function ListOfContacts(props){
-    let lstOfContacts = props['contactsList'];
-   // console.log(lstOfContacts);
-    return(
-        <ol className="contact-list">
-            {
-                lstOfContacts.map((person)=>(
-                    <li key={person.id} className="contact-list-item">
-
-                        
-                        <div className="contact-avatar" style={{
-                            backgroundImage : `url(${person.avatarURL})`
-
-                        }}>
-                        </div>
-                        
-                        <div className="contact-details">
-                            <p>{person.name}</p>
-                            <p>{person.handle}</p>
-
-                        </div>
-                        <button className="contact-remove"   onClick={()=>(
-                            (console.log('iam in remove contact'),
-                            props.onDeleteContact(person)))}>remove</button>
-                    </li>
-
-                ))
+//  use the class component instead of stateless function component to use state management feature of the react to handler the search feature
+class ListOfContacts extends Component{
+    // proptypes shall be static 
+    static propTypes={
+        contactsList : propTypes.array.isRequired,
+        onDeleteContact :propTypes.func.isRequired,
+    }
 
 
-            }
-        </ol>
-    )    
-}
-
-//use the proptypes to specify the props type will the component use
-
-ListOfContacts.propTypes = {
-    contactsList : propTypes.array.isRequired,
-    onDeleteContact :propTypes.func.isRequired,
+    // the render method
+    render(){
+        const lstOfContacts = this.props.contactsList;
+        return(
+            <ol className="contact-list">
+                {
+                    lstOfContacts.map((person)=>(
+                        <li key={person.id} className="contact-list-item">
     
+                            
+                            <div className="contact-avatar" style={{
+                                backgroundImage : `url(${person.avatarURL})`
+    
+                            }}>
+                            </div>
+                            
+                            <div className="contact-details">
+                                <p>{person.name}</p>
+                                <p>{person.handle}</p>
+    
+                            </div>
+                            <button className="contact-remove"   onClick={()=>(
+                                (console.log('iam in remove contact'),
+                                this.props.onDeleteContact(person)))}>remove</button>
+                        </li>
+    
+                    ))
+    
+    
+                }
+            </ol>
+        )    
+
+    }
+
+
 
 }
+
+
+
 
 
 export default ListOfContacts;
